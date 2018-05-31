31b74601768574d49509f2ac762fc62c6e9f962a
paul antezana <contacto@paulantezana.com>
2018-05-30 20:12:11 -0500

    new updates

--- models/matricula/monto.go
+++ models/matricula/monto.go
@@ -4,4 +4,7 @@ import "github.com/jinzhu/gorm"
 
 type Monto struct {
 	gorm.Model
+	Concepto string  `json:"concepto"`
+	Cantidad float32 `json:"cantidad"`
+	Estado   bool    `json:"estado" gorm:"default:'true'"`
 }
--- models/matricula/pago.go
+++ models/matricula/pago.go
@@ -4,4 +4,7 @@ import "github.com/jinzhu/gorm"
 
 type Pago struct {
 	gorm.Model
+	Concepto string  `json:"concepto"`
+	Cantidad float32 `json:"cantidad"`
+	Estado   bool    `json:"estado" gorm:"default:'true'"`
 }
