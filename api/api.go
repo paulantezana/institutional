c3a25e18ac6e1a928cab3394b8ec4875646ad0c9
paul antezana <contacto@paulantezana.com>
2018-06-02 18:06:15 -0500

    new updates

--- api/api.go
+++ api/api.go
@@ -1,86 +1,86 @@
 package api
 
 import (
-    "html/template"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
     "github.com/paulantezana/institutional/controller"
     "github.com/paulantezana/institutional/security"
-    "io"
-    "net/http"
 )
-
-// TemplateRenderer is a custom html/template renderer for Echo framework
-type TemplateRenderer struct {
-    templates *template.Template
-}
-
-// Render renders a template document
-func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-
-    // Add global methods if data is a map
-    if viewContext, isMap := data.(map[string]interface{}); isMap {
-        viewContext["reverse"] = c.Echo().Reverse
-    }
-
-    return t.templates.ExecuteTemplate(w, name, data)
-}
-
-
 
 
 // Start is starting api web service
 func Start() {
 	e := echo.New()
 
-    renderer := &TemplateRenderer{
-        templates: template.Must(template.ParseGlob("../public/*.html")),
-    }
-    e.Renderer = renderer
-
-    // Named route "foobar"
-    e.GET("/something", func(c echo.Context) error {
-        return c.Render(http.StatusOK, "../something.html", map[string]interface{}{
-            "name": "Dolly!",
-        })
-    }).Name = "foobar"
-
     // Middleware
     e.Use(middleware.Logger())
     e.Use(middleware.Recover())
 
-	// Middleware
-	//e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-	//	AllowOrigins: []string{"http://localhost:7070", "http://localhost:3005"},
-	//	AllowMethods: []string{echo.GET, echo.POST},
-	//}))
+    // Este midelware indica desde que 
+    // ips o nombres de dominio se consumiran la api
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:7070", "http://localhost:3005", "http://localhost"},
+		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE},
+    }))
+    
+    // Login del usuario de donde se optiene la clave token de seguridad
+    // para despues hacer toda las peticiones a la api
     e.POST("/login", controller.Login)
 
-	// Page template
-	e.Static("/","../public")
-
-    // Restricted group
+    // API: con proteccion de autenticacion mediante de tokens de seguridad
     api := e.Group("/api")
 
-    // Configure middleware with the custom claims type
+    // Configuracion del middleware con los claims personalizados
     config := middleware.JWTConfig{
     	Claims:     &security.Claim{},
     	SigningKey: []byte("secret"),
     }
     api.Use(middleware.JWTWithConfig(config))
 
+    // ==================================================================================================
+    // Toda las rutas del api
     // Routes =================================================== Usuario ===============================
     api.GET("/usuario", controller.GetUsuario)
-    e.POST("/usuario", controller.CreateUsuario)
-    e.PUT("/usuario", controller.UpdateUsuario)
-    e.DELETE("/usuario", controller.DeleteUsuario)
+    api.POST("/usuario", controller.CreateUsuario)
+    api.PUT("/usuario", controller.UpdateUsuario)
+    api.DELETE("/usuario", controller.DeleteUsuario)
+
+    // Routes =================================================== Carrera ===============================
+	api.GET("/instituto",controller.GetCarreras)
+    api.POST("/instituto", controller.CreateCarrera)
+    api.PUT("/instituto", controller.UpdateCarrera)
+    api.DELETE("/instituto", controller.DeleteCarrera)
+
+    // Routes =================================================== Carrera ===============================
+	api.GET("/filial",controller.GetCarreras)
+    api.POST("/filial", controller.CreateCarrera)
+    api.PUT("/filial", controller.UpdateCarrera)
+    api.DELETE("/filial", controller.DeleteCarrera)
 
 	// Routes =================================================== Carrera ===============================
-	e.GET("/carrera",controller.GetCarreras)
-    e.POST("/carrera", controller.CreateCarrera)
-    e.PUT("/carrera", controller.UpdateCarrera)
-    e.DELETE("/carrera", controller.DeleteCarrera)
+	api.GET("/carrera",controller.GetCarreras)
+    api.POST("/carrera", controller.CreateCarrera)
+    api.PUT("/carrera", controller.UpdateCarrera)
+    api.DELETE("/carrera", controller.DeleteCarrera)
 
-	// Init server
+    // Routes =================================================== Semestre ===============================
+	api.GET("/semestre",controller.GetCarreras)
+    api.POST("/semestre", controller.CreateCarrera)
+    api.PUT("/semestre", controller.UpdateCarrera)
+    api.DELETE("/semestre", controller.DeleteCarrera)
+
+    // Routes =================================================== Modulo ===============================
+	api.GET("/modulo",controller.GetCarreras)
+    api.POST("/modulo", controller.CreateCarrera)
+    api.PUT("/modulo", controller.UpdateCarrera)
+    api.DELETE("/modulo", controller.DeleteCarrera)
+
+    // Routes =================================================== Unidad ===============================
+	api.GET("/unidad",controller.GetCarreras)
+    api.POST("/unidad", controller.CreateCarrera)
+    api.PUT("/unidad", controller.UpdateCarrera)
+    api.DELETE("/unidad", controller.DeleteCarrera)
+
+    // Init server
 	e.Logger.Fatal(e.Start(":7070"))
 }
