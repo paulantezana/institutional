d65199a97815ca7ab2a1daa602d65318781b6401
paul antezana <contacto@paulantezana.com>
2018-06-11 06:04:04 -0700

    integracion heroku base de datos

--- config/config.go
+++ config/config.go
@@ -57,7 +57,7 @@ func GetConnection() *gorm.DB {
 	    dsn = fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", c.Database.User, c.Database.Pass, c.Database.Server, c.Database.Port, c.Database.Database)
     }else {
         // Mode Production in HEROKu
-        dsn = fmt.Sprintf("postgres://%s:%s@%s:%s/%s", c.Database.User, c.Database.Pass, c.Database.Server, c.Database.Port, c.Database.Database)
+        dsn = fmt.Sprintf("postgres://drvzldhzlyrtra:8a6a02fa8d55a65d5a370330d51b211aef066343e37d968eeb6a46527ba6ee48@ec2-54-225-107-174.compute-1.amazonaws.com:5432/d3mp2040aqlpme")
     }
 
     //dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", c.User, c.Pass, c.Server, c.Port, c.Database)
