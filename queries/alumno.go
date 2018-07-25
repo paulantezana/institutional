bed6be8879e8950905cd33c3c3cae6df729061c2
paul antezana <contacto@paulantezana.com>
2018-07-25 17:11:37 -0500

    new updates

--- queries/alumno.go
+++ queries/alumno.go
@@ -4,16 +4,32 @@ import (
 	"github.com/graphql-go/graphql"
 	"github.com/paulantezana/institutional/config"
 	"github.com/paulantezana/institutional/models"
+    "fmt"
 )
 
 func AlumnoQuery() *graphql.Field {
 	return &graphql.Field{
 		Type: graphql.NewList(models.AlumnoType),
+        Args: graphql.FieldConfigArgument{
+            "dni":              &graphql.ArgumentConfig{Type: graphql.String},
+            //"nombres":          &graphql.ArgumentConfig{Type: graphql.String},
+        },
 		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+		    a := models.Alumno{}
+            if p.Args["dni"] != nil {
+               a.Dni = p.Args["dni"].(string)
+            }
+
+            //if p.Args["nombres"] != nil {
+            //    alumno.Nombres = p.Args["nombres"].(string)
+            //}
+
 			db := config.GetConnection()
 			defer db.Close()
+
 			alumnos := make([]models.Alumno, 0)
-			if err := db.Find(&alumnos).Error; err != nil {
+
+			if err := db.Where("name LIKE ?", fmt.Sprint("%"+a.Dni+"%")).Find(&alumnos).Error; err != nil {
 				return nil, err
 			}
 			return alumnos, nil
